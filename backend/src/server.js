b775668b6efc06657479925c8433235e8e728910
Erick El nino <erick.tshimbombo@yahoo.com>
2024-05-06 17:26:58 +0200

    still setting up our environment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,1 +1,12 @@
-console.log("Server file is running ");
+const http = require("http");
+const dotenv = require("dotenv");
+const application = require("./app");
+
+dotenv.config();
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(application);
+
+server.listen(PORT, () => {
+  console.log(`http server is running on PORT : ${PORT}`);
+});
